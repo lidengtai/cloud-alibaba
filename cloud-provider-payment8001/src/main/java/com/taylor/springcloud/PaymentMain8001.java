72e61ab10dd496bc9acd982a547339ae91a67c38
taylor <lidengtai@163.com>
2021-02-27 23:18:20 +0800

    eureka相关配置：自我保护机制 心跳 等待时间上限 服务发现注解等

--- cloud-provider-payment8001/src/main/java/com/taylor/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/taylor/springcloud/PaymentMain8001.java
@@ -8,10 +8,13 @@ package com.taylor.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//服务发现注解
+@EnableDiscoveryClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
