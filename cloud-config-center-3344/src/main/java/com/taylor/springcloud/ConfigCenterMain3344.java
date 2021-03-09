55aa009bf6eb2a618b2a6c8717923a4f43e51d41
taylor <lidengtai@163.com>
2021-03-09 22:28:33 +0800

    spirng config 配置

---
+++ cloud-config-center-3344/src/main/java/com/taylor/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.taylor.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author taylor
+ * @Description: TODO
+ * @date 2021/3/8 23:18
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+
+}
---
+++ cloud-config-client-3355/src/main/java/com/taylor/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.taylor.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author taylor
+ * @Description: TODO
+ * @date 2021/3/9 0:23
+ */
+@RestController
+//动态刷新
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
